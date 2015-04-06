ebc5c7f21055d70f56335974934fd2e9f7105ceb
gearoid-oleary <gearoid.oleary@mycit.ie>
2015-04-07 00:34:14 +0100

    Added entry point to run java code...not yet working

---
+++ StudentApp/src/main/java/com/gearoidoleary/StudentApplication.java
@@ -0,0 +1,23 @@
+package com.gearoidoleary;
+
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//ClassPathXmlApplicationContext cp = new ClassPathXmlApplicationContext("beans.xml");
+		//StudentRepository studentRepository = cp.getBean(StudentRepository.class);
+		//List<Student> all = studentRepository.getAll();
+		//Cant get this piece working yet
+		System.out.println("Not working yet!");
+
+		
+		
+		
+	}
+
+}
